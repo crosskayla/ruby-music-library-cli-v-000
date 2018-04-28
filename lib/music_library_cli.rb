4590b3cbcca70a7321ccce954589ed01e2c8cffd
Kayla Cross <kac288@georgetown.edu>
2018-04-28 20:28:59 +0000

    automatically backed up by learn

--- lib/music_library_cli.rb
+++ lib/music_library_cli.rb
@@ -82,7 +82,6 @@ class MusicLibraryController
     l = list_songs
     song_number = gets.strip
     song = l[song_number-1] if song_number.is_a? Integer
-    binding.pry
     puts "Playing #{song.name} by #{song.artist.name}" if song
   end
 
