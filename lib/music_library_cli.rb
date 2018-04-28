89722191c6e453aacdf26334d76b2744e78086be
Kayla Cross <kac288@georgetown.edu>
2018-04-28 20:37:40 +0000

    automatically backed up by learn

--- lib/music_library_cli.rb
+++ lib/music_library_cli.rb
@@ -82,8 +82,9 @@ class MusicLibraryController
     song_index = gets.strip.to_i - 1
     l = Song.all.sort_by{|song| song.name}
     if song_index.between?(0, l.size-1)
-    song = l[song_index] if song_number.is_a? Integer
-    puts "Playing #{song.name} by #{song.artist.name}" if song
+      song = l[song_index] if song_number.is_a? Integer
+      puts "Playing #{song.name} by #{song.artist.name}" if song
+    end
   end
 
 end
