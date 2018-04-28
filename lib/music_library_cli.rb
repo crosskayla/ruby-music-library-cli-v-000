4527e3537795b01c79e57c1af92c2c5b7e55a192
Kayla Cross <kac288@georgetown.edu>
2018-04-28 20:11:24 +0000

    automatically backed up by learn

--- lib/music_library_cli.rb
+++ lib/music_library_cli.rb
@@ -44,7 +44,10 @@ class MusicLibraryController
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     artist_name = gets.strip
-    Artist.find_by_name(artist_name)
+    artist = Artist.find_by_name(artist_name)
+    artist.songs.each_with_index do |song, index|
+      puts "#{index+1}. #{song.name}"
+    end
   end
 
   def list_songs_by_genre
