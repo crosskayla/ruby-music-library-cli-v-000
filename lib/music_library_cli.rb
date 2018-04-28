3bf8b977d29cb55e49d92dbc7ef6790c04ceb447
Kayla Cross <kac288@georgetown.edu>
2018-04-28 20:41:36 +0000

    automatically backed up by learn

--- lib/music_library_cli.rb
+++ lib/music_library_cli.rb
@@ -16,18 +16,18 @@ class MusicLibraryController
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
     input = gets.strip until input == "exit"
-    case input
-    when "list songs"
-      list_songs
-    when "list artists"
-      list_artists
-    when "list genres"
-      list_genres
-    when "list artist"
-      list_songs_by_artist
-    when "list genre"
-      list_songs_by_genre
-    end
+    # case input
+    # when "list songs"
+    #   list_songs
+    # when "list artists"
+    #   list_artists
+    # when "list genres"
+    #   list_genres
+    # when "list artist"
+    #   list_songs_by_artist
+    # when "list genre"
+    #   list_songs_by_genre
+    # end
   end
 
   def list_songs
