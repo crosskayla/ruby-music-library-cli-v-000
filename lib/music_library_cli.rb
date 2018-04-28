85443a624d0be80b3ee3c4ac6eb357597db6f6af
Kayla Cross <kac288@georgetown.edu>
2018-04-28 20:48:37 +0000

    automatically backed up by learn

--- lib/music_library_cli.rb
+++ lib/music_library_cli.rb
@@ -15,7 +15,8 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    loop do input = gets.strip
+    loop do
+      input = gets.strip
       case input
       when "list songs"
         list_songs
